0a35443fe5f4a715925b1b934f829395f280383f
jayjin <jayjinjay@gmail.com>
2018-01-17 15:36:30 +0900

    #3 Apply LESS compiler without ejecting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Button from "antd/lib/button";
+import { Layout, Menu, Icon } from "antd";
+import "./App.css";
+const { Header, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout>
+          <Sider breakpoint="sm" collapsedWidth="0">
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span>nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span>nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span>nav 3</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header>
+              <img src={logo} className="App-logo" alt="logo" />NOOTE
+            </Header>
+            <Content>
+              Content
+              
+            </Content>
+          </Layout>
+        </Layout>
       </div>
     );
   }
