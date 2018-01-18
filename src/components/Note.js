1b4bc8ccc752b3d0b3cba134e4d9ef8657a6d44d
jayjin <jayjinjay@gmail.com>
2018-01-18 18:54:29 +0900

    #4 Add dropdown action to Note & add markdown editor(simpleMDE)

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Menu, Dropdown, Icon } from 'antd';
 
 export default class extends Component {
+  renderDropdown() {
+    
+  }
+
   render() {
     const { noteData } = this.props;
+
+    const menu = (
+      <Menu>
+        <Menu.Item key="0">Move to TODO</Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="3">Delete Note</Menu.Item>
+      </Menu>
+    );
 
     return (
       <Link to={`/note/${noteData.id}/`}>
@@ -12,6 +25,11 @@ export default class extends Component {
             <div className="title">{noteData.title}</div>
             <div className="contents">{noteData.contents}</div>
             <div className="date">{noteData.date}</div>
+            <Dropdown overlay={menu}>
+              <div className="ant-dropdown-link">
+                <Icon type="down" />
+              </div>
+            </Dropdown>
           </div>
         </div>
       </Link>
--- src/containers/NotePage.js
+++ src/containers/NotePage.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import { fetchNoteDetail } from "../actions/index";
 import { connect } from "react-redux";
-import { NoteList } from "../components";
+import SimpleMDE from "react-simplemde-editor";
+import "react-simplemde-editor/dist/simplemde.min.css";
 
 class NotePage extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      noteValue: ''
+    };
+  }
+
   componentDidMount() {
     const noteId = this.props.match.params.noteId;
-    this.props.fetchNoteDetail(noteId);
+    this.props.fetchNoteDetail(noteId).then(() => {
+      this.setState({noteValue: this.props.noteDetail.contents});
+    });
   }
 
   /* Catch route change event */
-  componentWillReceiveProps (nextProps) {
-    if ( this.props.location.pathname !== nextProps.location.pathname ) {
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
       const noteId = nextProps.match.params.noteId;
       this.props.fetchNoteDetail(noteId);
     }
@@ -19,18 +29,21 @@ class NotePage extends Component {
 
   render() {
     const { noteDetail } = this.props;
-    
+
     return (
       <div>
         {noteDetail && (
           <div>
-            <section className="titleSection">
+            <section className="noteTitleSection">
               <h2 className="headtitle">{noteDetail.title}</h2>
               <span>{noteDetail.date}</span>
               <span>{noteDetail.notebookTitle}</span>
             </section>
             <section className="contentsSection">
-              {noteDetail.contents}
+              <SimpleMDE
+                value={this.state.noteValue}
+                options={{ spellChecker: false }}
+              />
             </section>
           </div>
         )}
