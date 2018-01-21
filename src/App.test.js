2136fe88197b10f24449712a3e2fc7f0c3ea4698
jayjin <jayjinjay@gmail.com>
2018-01-21 18:05:02 +0900

    #7 Set up test(jest, enzyme)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import Note from './components/Note'
+import { Link } from 'react-router-dom';
+import { StaticRouter } from 'react-router'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+  const props = {
+    noteData: jest.fn()
+  }
+
+  const component = (
+    <StaticRouter location="someLocation">
+      <Note {...props} />
+    </StaticRouter>
+  );
+  
+  // const enzymeWrapper = mount(<Note {...props} />)
+  const enzymeWrapper = mount(component)
+
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
+describe('components', () => {
+  
+
+
+  describe('Note', () => {
+    it('should render self and subcomponents', () => {
+      const { enzymeWrapper } = setup()
+      // expect(enzymeWrapper.find('header').hasClass('header')).toBe(true)
+      expect(enzymeWrapper.find('h1').text()).toBe('Hi');
+      expect(enzymeWrapper.find('h1').text()).toBe('Hi2');
+      // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
+      // expect(todoInputProps.newTodo).toBe(true)
+      // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+    })
+
+    // it('should call addTodo if length of text is greater than 0', () => {
+    //   const { enzymeWrapper, props } = setup()
+    //   const input = enzymeWrapper.find('TodoTextInput')
+    //   input.props().onSave('')
+    //   expect(props.addTodo.mock.calls.length).toBe(0)
+    //   input.props().onSave('Use Redux')
+    //   expect(props.addTodo.mock.calls.length).toBe(1)
+    // })
+  })
+})
